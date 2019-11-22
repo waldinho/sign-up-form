4d925c7997bebe7f44b05ce23ac8715d4b26e064
waldinho <james.waller1984@gmail.com>
2019-11-22 17:12:38 +1100

    readme updated and app prod build.

---
+++ build/precache-manifest.7e2bda78ba6f908bc964a3626137ecf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f1fd2f83ca165cd8fdeb4e2015e5c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91cb3ba6a69a5aa952a2",
+    "url": "/static/css/main.15447199.chunk.css"
+  },
+  {
+    "revision": "ff5e90dcb24d5daba2fc",
+    "url": "/static/js/2.7fb81a9a.chunk.js"
+  },
+  {
+    "revision": "91cb3ba6a69a5aa952a2",
+    "url": "/static/js/main.8e0c502f.chunk.js"
+  },
+  {
+    "revision": "4093202bf237b14674e7",
+    "url": "/static/js/runtime-main.8b590eb8.js"
+  }
+]);
